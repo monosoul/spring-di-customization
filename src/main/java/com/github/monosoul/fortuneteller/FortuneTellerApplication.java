5a79fec5a1ab36754332fa9783dd7c9dee90318f
monosoul <kloz.klaud@gmail.com>
2018-09-15 16:54:52 +0500

    initial app commit

---
+++ src/main/java/com/github/monosoul/fortuneteller/FortuneTellerApplication.java
@@ -0,0 +1,11 @@
+package com.github.monosoul.fortuneteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneTellerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FortuneTellerApplication.class, args);
+    }
+}
