e2273cd7b62cfc31e6aeb95538fa31d90ac5742d
monosoul <kloz.klaud@gmail.com>
2018-09-23 20:05:51 +0500

    PersonalDataExtractor: add request validation

--- src/main/java/com/github/monosoul/fortuneteller/domain/PersonalDataExtractor.java
+++ src/main/java/com/github/monosoul/fortuneteller/domain/PersonalDataExtractor.java
@@ -2,14 +2,27 @@ package com.github.monosoul.fortuneteller.domain;
 
 import com.github.monosoul.fortuneteller.model.FortuneRequest;
 import com.github.monosoul.fortuneteller.model.PersonalData;
+import lombok.NonNull;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 @Component
 public final class PersonalDataExtractor implements Function<FortuneRequest, PersonalData> {
+
+    private final Predicate<FortuneRequest> requestValidator;
+
+    public PersonalDataExtractor(@NonNull final Predicate<FortuneRequest> requestValidator) {
+        this.requestValidator = requestValidator;
+    }
+
     @Override
     public PersonalData apply(final FortuneRequest request) {
+        if (!requestValidator.test(request)) {
+            throw new IllegalArgumentException("Invalid request!");
+        }
+
         return PersonalData.builder()
                 .name(request.getName())
                 .age(request.getAge())
