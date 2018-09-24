09bfa8080cb02ded578c6c0774d1dccb4dd056b4
monosoul <kloz.klaud@gmail.com>
2018-09-24 22:27:09 +0500

    introduce ZodiacSign enum

---
+++ src/main/java/com/github/monosoul/fortuneteller/common/ZodiacSign.java
@@ -0,0 +1,16 @@
+package com.github.monosoul.fortuneteller.common;
+
+public enum ZodiacSign {
+    ARIES,
+    TAURUS,
+    GEMINI,
+    CANCER,
+    LEO,
+    VIRGO,
+    LIBRA,
+    SCORPIO,
+    SAGITTARIUS,
+    CAPRICORN,
+    AQUARIUS,
+    PISCES
+}
