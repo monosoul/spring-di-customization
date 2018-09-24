167f8e5d64c040e72f062227f926875a91148b47
monosoul <kloz.klaud@gmail.com>
2018-09-24 22:06:55 +0500

    FortuneTellControllerTestFT: add test for the truth

--- src/test/java/com/github/monosoul/fortuneteller/test/functional/FortuneTellControllerTestFT.java
+++ src/test/java/com/github/monosoul/fortuneteller/test/functional/FortuneTellControllerTestFT.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static com.github.monosoul.fortuneteller.aspect.TellTheTruthAspect.THE_TRUTH;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -45,6 +46,26 @@ public class FortuneTellControllerTestFT {
         assertThat(actual.getStatusCode()).isEqualByComparingTo(OK);
         assertThat(actual.getBody()).isNotNull();
         assertThat(actual.getBody().getMessage()).isNotBlank();
+
+        LOGGER.info("Received response: {}", actual.getBody());
+    }
+
+    //@Test
+    void testWithAspect() {
+        val actual = client.postForEntity(
+                "http://localhost:" + port + "/fortune/tell",
+                FortuneRequest.builder()
+                        .name("Name")
+                        .zodiacSign("Sign")
+                        .age(100)
+                        .email("someone@somewhere.fake")
+                        .build(),
+                FortuneResponse.class
+        );
+
+        assertThat(actual.getStatusCode()).isEqualByComparingTo(OK);
+        assertThat(actual.getBody()).isNotNull();
+        assertThat(actual.getBody().getMessage()).isEqualTo(THE_TRUTH);
 
         LOGGER.info("Received response: {}", actual.getBody());
     }
