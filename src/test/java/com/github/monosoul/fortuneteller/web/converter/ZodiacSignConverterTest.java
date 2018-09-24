ca4a752ae0a409bcba5d890eb69206ab6887a284
monosoul <kloz.klaud@gmail.com>
2018-09-24 23:22:08 +0500

    ZodiacSignConverterTest: use randomEnum()

--- src/test/java/com/github/monosoul/fortuneteller/web/converter/ZodiacSignConverterTest.java
+++ src/test/java/com/github/monosoul/fortuneteller/web/converter/ZodiacSignConverterTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static com.github.monosoul.fortuneteller.util.Util.randomEnum;
 import static java.util.stream.Stream.generate;
 import static org.apache.commons.lang3.RandomUtils.nextBoolean;
-import static org.apache.commons.lang3.RandomUtils.nextInt;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ZodiacSignConverterTest {
@@ -34,6 +34,6 @@ class ZodiacSignConverterTest {
     }
 
     private static Stream<ZodiacSign> zodiacSignStream() {
-        return generate(() -> ZodiacSign.values()[nextInt(0, ZodiacSign.values().length)]).limit(LIMIT);
+        return generate(() -> randomEnum(ZodiacSign.class)).limit(LIMIT);
     }
 }
