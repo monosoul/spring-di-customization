53fe4163a084b709c4e25146a4219338a1fa01bd
monosoul <kloz.klaud@gmail.com>
2018-09-25 17:00:35 +0500

    introduce LoggingFortuneTeller

---
+++ src/main/java/com/github/monosoul/fortuneteller/domain/impl/LoggingFortuneTeller.java
@@ -0,0 +1,33 @@
+package com.github.monosoul.fortuneteller.domain.impl;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import com.github.monosoul.fortuneteller.domain.FortuneTeller;
+import com.github.monosoul.fortuneteller.model.FortuneRequest;
+import com.github.monosoul.fortuneteller.model.FortuneResponse;
+import lombok.NonNull;
+import lombok.val;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggingFortuneTeller implements FortuneTeller {
+
+    private final FortuneTeller internal;
+    private final Logger logger;
+
+    public LoggingFortuneTeller(@NonNull final FortuneTeller internal) {
+        this.internal = internal;
+        this.logger = getLogger(internal.getClass());
+    }
+
+    @Override
+    public FortuneResponse tell(final FortuneRequest request) {
+        logger.info("Got request: {}", request);
+
+        val response = internal.tell(request);
+
+        logger.info("Going to answer: {}", response);
+
+        return response;
+    }
+}
---
+++ src/test/java/com/github/monosoul/fortuneteller/domain/impl/LoggingFortuneTellerTest.java
@@ -0,0 +1,68 @@
+package com.github.monosoul.fortuneteller.domain.impl;
+
+import static ch.qos.logback.classic.Level.DEBUG;
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.slf4j.LoggerFactory.getLogger;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+import com.github.monosoul.fortuneteller.domain.FortuneTeller;
+import com.github.monosoul.fortuneteller.model.FortuneRequest;
+import com.github.monosoul.fortuneteller.model.FortuneResponse;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+
+class LoggingFortuneTellerTest {
+
+    @Mock
+    private FortuneTeller internal;
+    @Mock
+    private FortuneRequest request;
+    @Mock
+    private FortuneResponse response;
+    @Mock
+    private Appender<ILoggingEvent> appender;
+    @Captor
+    private ArgumentCaptor<ILoggingEvent> captor;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+
+        when(internal.tell(request)).thenReturn(response);
+        configureLogger();
+    }
+
+    @Test
+    void tell() {
+        val actual = new LoggingFortuneTeller(internal).tell(request);
+
+        verify(internal).tell(request);
+        verify(appender, times(2)).doAppend(captor.capture());
+        verifyNoMoreInteractions(internal, appender);
+
+        assertThat(actual).isSameAs(response);
+
+        val events = captor.getAllValues().stream().map(ILoggingEvent::getFormattedMessage);
+        assertThat(events).containsExactly(
+                format("Got request: %s", request),
+                format("Going to answer: %s", response)
+        );
+    }
+
+    private void configureLogger() {
+        val logger = (Logger) getLogger(internal.getClass());
+        logger.addAppender(appender);
+        logger.setLevel(DEBUG);
+    }
+}
