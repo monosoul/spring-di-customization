9ac383ca8424ee197a1e2b25abac1638baee0f3a
monosoul <kloz.klaud@gmail.com>
2018-09-25 23:17:08 +0500

    fully implement DecoratorAutowireCandidateResolver

--- src/main/java/com/github/monosoul/fortuneteller/spring/DecoratorAutowireCandidateResolver.java
+++ src/main/java/com/github/monosoul/fortuneteller/spring/DecoratorAutowireCandidateResolver.java
@@ -3,6 +3,7 @@ package com.github.monosoul.fortuneteller.spring;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import lombok.var;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.config.DependencyDescriptor;
 import org.springframework.beans.factory.support.AutowireCandidateResolver;
@@ -13,10 +14,13 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import static com.github.monosoul.fortuneteller.spring.Decorator.NOT_DECORATOR;
+
 @Slf4j
 class DecoratorAutowireCandidateResolver implements AutowireCandidateResolver {
 
     private final Map<Decorator, Set<String>> dependencies = new HashMap<>();
+    private final Set<String> decoratorsWithFoundDependencies = new HashSet<>();
     private final AutowireCandidateResolver resolver;
 
     DecoratorAutowireCandidateResolver(@NonNull final DefaultListableBeanFactory beanFactory) {
@@ -26,7 +30,32 @@ class DecoratorAutowireCandidateResolver implements AutowireCandidateResolver {
 
     @Override
     public boolean isAutowireCandidate(final BeanDefinitionHolder bdHolder, final DependencyDescriptor descriptor) {
-        return false;
+        val dependentClass = getDependentClass(descriptor);
+        val dependentClassName = dependentClass.getName();
+        log.debug("Looking for dependencies for {}", dependentClassName);
+
+        if (!isSameTypeAsDependent(descriptor)) {
+            log.debug(
+                    "Dependency type differs from dependent type. Delegating to internal resolver ({})",
+                    resolver.getClass().getName()
+            );
+            return resolver.isAutowireCandidate(bdHolder, descriptor);
+        }
+        if (decoratorsWithFoundDependencies.contains(dependentClassName)) {
+            log.debug("Already found dependency for {}. Skipping.", dependentClassName);
+            return false;
+        }
+
+        primaryIfTopLevel(bdHolder, dependentClass);
+        if (isChildDecorator(bdHolder, dependentClass)) {
+            return true;
+        }
+        log.debug(
+                "Candidate is not child decorator. Falling back to internal resolver ({})",
+                resolver.getClass().getName()
+        );
+
+        return resolver.isAutowireCandidate(bdHolder, descriptor);
     }
 
     @Override
@@ -69,5 +98,69 @@ class DecoratorAutowireCandidateResolver implements AutowireCandidateResolver {
 
     private String getSimpleName(@NonNull final String beanClassName) {
         return beanClassName.replaceAll(".*\\.", "");
+    }
+
+    private Class<?> getDependentClass(final DependencyDescriptor descriptor) {
+        return descriptor.getMember().getDeclaringClass();
+    }
+
+    private boolean isSameTypeAsDependent(final DependencyDescriptor descriptor) {
+        val dependent = getDependentClass(descriptor);
+
+        return descriptor.getDependencyType().isAssignableFrom(dependent);
+    }
+
+    private Decorator getDecoratorTypeFor(final Class<?> clazz) {
+        for (val decoratorType : Decorator.values()) {
+            if (clazz.getSimpleName().startsWith(decoratorType.getPrefix())) {
+                return decoratorType;
+            }
+        }
+
+        return NOT_DECORATOR;
+    }
+
+    private boolean isChildDecorator(final BeanDefinitionHolder bdHolder, final Class<?> dependentClass) {
+        val dependentClassName = dependentClass.getName();
+
+        val candidateBeanName = bdHolder.getBeanName();
+        log.debug("Candidate bean name is {}", candidateBeanName);
+
+        val dependentDecoratorType = getDecoratorTypeFor(dependentClass);
+        val decoratorTypes = Decorator.values();
+        val lastDecoratorTypeIndex = decoratorTypes.length - 1;
+        if (dependentDecoratorType.ordinal() == lastDecoratorTypeIndex) {
+            log.debug("{} of decorator type {} has same type dependency. Probably not decorator. Falling back to " +
+                    "internal resolver ({})", dependentClassName, dependentDecoratorType, resolver.getClass().getName());
+            return false;
+        }
+
+        for (var i = dependentDecoratorType.ordinal(); i < decoratorTypes.length; i++) {
+            if (i == lastDecoratorTypeIndex) {
+                break;
+            }
+            val dependencyDecoratorType = decoratorTypes[i + 1];
+            log.debug("Looking for dependencies of decorator type {}", dependencyDecoratorType);
+            if (dependencies.get(dependencyDecoratorType).contains(candidateBeanName)) {
+                log.debug(
+                        "{} is a decorator type {} dependency for {}",
+                        candidateBeanName, dependencyDecoratorType, dependentClassName
+                );
+                decoratorsWithFoundDependencies.add(dependentClassName);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void primaryIfTopLevel(final BeanDefinitionHolder bdHolder, final Class<?> dependentClass) {
+        val dependentClassName = dependentClass.getName();
+        val dependentDecoratorType = getDecoratorTypeFor(dependentClass);
+        log.debug("{} is {} decorator", dependentClassName, dependentDecoratorType);
+        if (dependentDecoratorType.ordinal() == 0) {
+            log.debug("{} is top level decorator. Making it primary.", dependentClassName);
+            bdHolder.getBeanDefinition().setPrimary(true);
+        }
     }
 }
