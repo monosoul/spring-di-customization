4bc6e9d8ef383cb00aa7115f477a3d2bdc45e50f
monosoul <kloz.klaud@gmail.com>
2018-09-25 17:20:07 +0500

    CacheFactoryBean: use concurrent HashMap

--- src/main/java/com/github/monosoul/fortuneteller/domain/impl/CacheFactoryBean.java
+++ src/main/java/com/github/monosoul/fortuneteller/domain/impl/CacheFactoryBean.java
@@ -2,8 +2,8 @@ package com.github.monosoul.fortuneteller.domain.impl;
 
 import com.github.monosoul.fortuneteller.model.FortuneRequest;
 import com.github.monosoul.fortuneteller.model.FortuneResponse;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +12,11 @@ public class CacheFactoryBean implements FactoryBean<Map<FortuneRequest, Fortune
 
     @Override
     public Map<FortuneRequest, FortuneResponse> getObject() {
-        return new HashMap<>();
+        return new ConcurrentHashMap<>();
     }
 
     @Override
     public Class<?> getObjectType() {
-        return Map.class;
+        return ConcurrentHashMap.class;
     }
 }
--- src/test/java/com/github/monosoul/fortuneteller/domain/impl/CacheFactoryBeanTest.java
+++ src/test/java/com/github/monosoul/fortuneteller/domain/impl/CacheFactoryBeanTest.java
@@ -1,19 +1,18 @@
 package com.github.monosoul.fortuneteller.domain.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.junit.jupiter.api.Test;
 
 class CacheFactoryBeanTest {
 
     @Test
     void getObject() {
-        assertThat(new CacheFactoryBean().getObject()).isEqualTo(new HashMap<>());
+        assertThat(new CacheFactoryBean().getObject()).isEqualTo(new ConcurrentHashMap<>());
     }
 
     @Test
     void getObjectType() {
-        assertThat(new CacheFactoryBean().getObjectType()).isEqualTo(Map.class);
+        assertThat(new CacheFactoryBean().getObjectType()).isEqualTo(ConcurrentHashMap.class);
     }
 }
