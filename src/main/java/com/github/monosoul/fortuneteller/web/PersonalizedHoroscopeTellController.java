f98934c2ba6b5ba26c24751d95cc63223307d88e
monosoul <kloz.klaud@gmail.com>
2018-09-25 13:59:57 +0500

    introduce PersonalizedHoroscopeTellController

---
+++ src/main/java/com/github/monosoul/fortuneteller/web/PersonalizedHoroscopeTellController.java
@@ -0,0 +1,52 @@
+package com.github.monosoul.fortuneteller.web;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import com.github.monosoul.fortuneteller.common.ZodiacSign;
+import com.github.monosoul.fortuneteller.domain.HoroscopeTeller;
+import com.github.monosoul.fortuneteller.model.PersonalizedHoroscope;
+import java.util.function.Function;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(
+        value = "/horoscope",
+        produces = APPLICATION_JSON_UTF8_VALUE
+)
+@Slf4j
+public class PersonalizedHoroscopeTellController {
+
+    private final HoroscopeTeller horoscopeTeller;
+    private final Function<String, ZodiacSign> zodiacSignConverter;
+    private final Function<String, String> nameNormalizer;
+
+    public PersonalizedHoroscopeTellController(
+            @NonNull final HoroscopeTeller horoscopeTeller,
+            @NonNull final Function<String, ZodiacSign> zodiacSignConverter,
+            @NonNull final Function<String, String> nameNormalizer
+    ) {
+        this.horoscopeTeller = horoscopeTeller;
+        this.zodiacSignConverter = zodiacSignConverter;
+        this.nameNormalizer = nameNormalizer;
+    }
+
+    @GetMapping(value = "/tell/name={name}&sign={sign}")
+    public PersonalizedHoroscope tell(@PathVariable final String name, @PathVariable final String sign) {
+        log.info("Received name: {}; sign: {}", name, sign);
+
+        return PersonalizedHoroscope.builder()
+                                    .name(
+                                            nameNormalizer.apply(name)
+                                    )
+                                    .horoscope(
+                                            horoscopeTeller.tell(
+                                                    zodiacSignConverter.apply(sign)
+                                            )
+                                    )
+                                    .build();
+    }
+}
---
+++ src/test/java/com/github/monosoul/fortuneteller/web/PersonalizedHoroscopeTellControllerTest.java
@@ -0,0 +1,71 @@
+package com.github.monosoul.fortuneteller.web;
+
+import static java.util.stream.Stream.generate;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import com.github.monosoul.fortuneteller.common.ZodiacSign;
+import com.github.monosoul.fortuneteller.domain.HoroscopeTeller;
+import com.github.monosoul.fortuneteller.model.Horoscope;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+
+class PersonalizedHoroscopeTellControllerTest {
+
+    private static final int LIMIT = 10;
+
+    @Mock
+    private HoroscopeTeller horoscopeTeller;
+    @Mock
+    private Function<String, ZodiacSign> zodiacSignConverter;
+    @Mock
+    private Function<String, String> nameNormalizer;
+    @Mock
+    private Horoscope horoscope;
+    @Mock
+    private ZodiacSign zodiacSign;
+    private String normalizedName;
+    private PersonalizedHoroscopeTellController controller;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+
+        normalizedName = randomAlphabetic(LIMIT);
+        controller = new PersonalizedHoroscopeTellController(horoscopeTeller, zodiacSignConverter, nameNormalizer);
+
+        when(horoscopeTeller.tell(any(ZodiacSign.class))).thenReturn(horoscope);
+        when(zodiacSignConverter.apply(anyString())).thenReturn(zodiacSign);
+        when(nameNormalizer.apply(anyString())).thenReturn(normalizedName);
+    }
+
+    @ParameterizedTest
+    @MethodSource("nameAndSignStream")
+    void tell(final String name, final String sign) {
+        val actual = controller.tell(name, sign);
+
+        verify(nameNormalizer).apply(name);
+        verify(zodiacSignConverter).apply(sign);
+        verify(horoscopeTeller).tell(zodiacSign);
+        verifyNoMoreInteractions(nameNormalizer, zodiacSignConverter, horoscopeTeller);
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.getName()).isSameAs(normalizedName);
+        assertThat(actual.getHoroscope()).isSameAs(horoscope);
+    }
+
+    private static Stream<Arguments> nameAndSignStream() {
+        return generate(() -> (Arguments) () -> new Object[]{randomAlphabetic(LIMIT), randomAlphabetic(LIMIT)}).limit(LIMIT);
+    }
+}
