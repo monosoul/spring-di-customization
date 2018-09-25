fde2ab56b340a6ed89f6e1fd3a3e8c7a19ef0ce4
monosoul <kloz.klaud@gmail.com>
2018-09-25 14:04:36 +0500

    TellTheTruthAspect: add advice for the PersonalizedHoroscope controller

--- src/main/java/com/github/monosoul/fortuneteller/aspect/TellTheTruthAspect.java
+++ src/main/java/com/github/monosoul/fortuneteller/aspect/TellTheTruthAspect.java
@@ -2,6 +2,7 @@ package com.github.monosoul.fortuneteller.aspect;
 
 import com.github.monosoul.fortuneteller.model.FortuneResponse;
 import com.github.monosoul.fortuneteller.model.Horoscope;
+import com.github.monosoul.fortuneteller.model.PersonalizedHoroscope;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -33,5 +34,18 @@ public class TellTheTruthAspect {
         joinPoint.proceed();
 
         return Horoscope.builder().message(THE_TRUTH).build();
+    }
+
+    @Around("@annotation(com.github.monosoul.fortuneteller.aspect.TellTheTruth)&&" +
+            "execution(public com.github.monosoul.fortuneteller.model.PersonalizedHoroscope " +
+            "com.github.monosoul.fortuneteller.web..*(*))")
+    public PersonalizedHoroscope tellTheTruthPersonalizedHoroscope(final ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Forcing the truth...");
+        joinPoint.proceed();
+
+        return PersonalizedHoroscope.builder()
+                                    .name("Anonymous")
+                                    .horoscope(Horoscope.builder().message(THE_TRUTH).build())
+                                    .build();
     }
 }
