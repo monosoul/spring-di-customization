974f3c88ab5b6ad1a076c5fa898a2bd9bcaefb03
monosoul <kloz.klaud@gmail.com>
2018-09-25 14:30:06 +0500

    add spring profile for integration tests

--- src/test/java/com/github/monosoul/fortuneteller/test/integration/TestITConfiguration.java
+++ src/test/java/com/github/monosoul/fortuneteller/test/integration/TestITConfiguration.java
@@ -2,8 +2,10 @@ package com.github.monosoul.fortuneteller.test.integration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("integration")
 @ComponentScan({"com.github.monosoul.fortuneteller.domain", "com.github.monosoul.fortuneteller.da"})
 public class TestITConfiguration {
 }
