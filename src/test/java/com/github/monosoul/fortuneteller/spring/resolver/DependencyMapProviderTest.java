952c0ced2ff052e8479d30ad30a600a423268e1b
monosoul <kloz.klaud@gmail.com>
2018-09-26 11:42:38 +0500

    introduce DependencyMapProvider

---
+++ src/test/java/com/github/monosoul/fortuneteller/spring/resolver/DependencyMapProviderTest.java
@@ -0,0 +1,86 @@
+package com.github.monosoul.fortuneteller.spring.resolver;
+
+import static com.github.monosoul.fortuneteller.util.Util.randomEnum;
+import static java.util.stream.Stream.generate;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import com.github.monosoul.fortuneteller.spring.DecoratorType;
+import java.util.stream.Stream;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+class DependencyMapProviderTest {
+
+    private static final int LIMIT = 10;
+
+    @Mock
+    private ConfigurableListableBeanFactory beanFactory;
+    @Mock
+    private BeanDefinition beanDefinition;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+
+        when(beanFactory.getBeanDefinition(anyString())).thenReturn(beanDefinition);
+    }
+
+    @Test
+    void skipWhenBeanClassNameIsNull() {
+        val beanName = randomAlphabetic(LIMIT);
+
+        when(beanFactory.getBeanDefinitionNames()).thenReturn(new String[]{beanName});
+        when(beanDefinition.getBeanClassName()).thenReturn(null);
+
+        val actual = new DependencyMapProvider().apply(beanFactory);
+
+        verify(beanFactory).getBeanDefinitionNames();
+        verify(beanFactory).getBeanDefinition(beanName);
+        verifyNoMoreInteractions(beanFactory);
+
+        assertThat(actual).isEmpty();
+    }
+
+    @ParameterizedTest
+    @MethodSource("decoratorTypeBeanNameAndClassNameStream")
+    void apply(final DecoratorType decoratorType, final String beanName, final String beanClassName) {
+        when(beanFactory.getBeanDefinitionNames()).thenReturn(new String[]{beanName});
+        when(beanDefinition.getBeanClassName()).thenReturn(beanClassName);
+
+        val actual = new DependencyMapProvider().apply(beanFactory);
+
+        verify(beanFactory).getBeanDefinitionNames();
+        verify(beanFactory).getBeanDefinition(beanName);
+        verifyNoMoreInteractions(beanFactory);
+
+        assertThat(actual).isNotNull()
+                          .hasSize(1);
+        assertThat(actual.get(decoratorType)).isNotNull()
+                                             .hasSize(1)
+                                             .containsExactly(beanName);
+    }
+
+    private static Stream<Arguments> decoratorTypeBeanNameAndClassNameStream() {
+        return generate(() -> (Arguments) () -> {
+                    val decoratorType = randomEnum(DecoratorType.class);
+                    return new Object[]{
+                            decoratorType,
+                            randomAlphabetic(LIMIT),
+                            decoratorType.getPrefix() + randomAlphabetic(LIMIT)
+                    };
+                }
+        ).limit(LIMIT);
+    }
+}
