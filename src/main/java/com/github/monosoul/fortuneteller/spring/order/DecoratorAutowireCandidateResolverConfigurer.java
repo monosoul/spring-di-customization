fd268a0215da36871ff746bae30a3146f0d01fda
monosoul <kloz.klaud@gmail.com>
2018-10-25 17:34:03 +0500

    introduce DecoratorAutowireCandidateResolverConfigurer

---
+++ src/main/java/com/github/monosoul/fortuneteller/spring/order/DecoratorAutowireCandidateResolverConfigurer.java
@@ -0,0 +1,24 @@
+package com.github.monosoul.fortuneteller.spring.order;
+
+import lombok.val;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class DecoratorAutowireCandidateResolverConfigurer implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(final ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        Assert.state(configurableListableBeanFactory instanceof DefaultListableBeanFactory,
+                "BeanFactory needs to be a DefaultListableBeanFactory");
+        val beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
+
+        beanFactory.setAutowireCandidateResolver(
+                new DecoratorAutowireCandidateResolver(beanFactory.getAutowireCandidateResolver())
+        );
+    }
+}
