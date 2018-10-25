2a42bfd608f2d2c209e1a5d5d11abc06624da04f
monosoul <kloz.klaud@gmail.com>
2018-10-25 22:47:58 +0500

    OrderConfigurer: refactoring

--- src/main/java/com/github/monosoul/fortuneteller/spring/order/OrderConfigurer.java
+++ src/main/java/com/github/monosoul/fortuneteller/spring/order/OrderConfigurer.java
@@ -1,21 +1,17 @@
 package com.github.monosoul.fortuneteller.spring.order;
 
-import static org.springframework.beans.factory.support.AbstractBeanDefinition.AUTOWIRE_AUTODETECT;
-import static org.springframework.beans.factory.support.BeanDefinitionBuilder.genericBeanDefinition;
+import java.util.function.Consumer;
 import lombok.val;
-import lombok.var;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
-import org.springframework.beans.factory.support.AutowireCandidateQualifier;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 @Component
-public class OrderConfigurer implements BeanFactoryPostProcessor {
+public final class OrderConfigurer implements BeanFactoryPostProcessor {
 
     @Override
     public void postProcessBeanFactory(final ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
@@ -23,33 +19,13 @@ public class OrderConfigurer implements BeanFactoryPostProcessor {
                 "BeanFactory needs to be a DefaultListableBeanFactory");
         val beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
 
+        val orderConfigProcessor = orderConfigProcessor(beanFactory);
+
         beanFactory.getBeansOfType(OrderConfig.class)
-                   .forEach((key, value) -> processOrderConfig(value, beanFactory));
+                   .forEach((key, value) -> orderConfigProcessor.accept(value));
     }
 
-    private void processOrderConfig(final OrderConfig<?> orderConfig, final BeanDefinitionRegistry beanFactory) {
-        var previousBeanName = "";
-        for (var i = 0; i < orderConfig.getClasses().length; i++) {
-            val currentBeanClass = orderConfig.getClasses()[i];
-            val currentBeanName = currentBeanClass.getTypeName();
-
-            beanFactory.registerBeanDefinition(
-                    currentBeanName,
-                    createBeanDefinition(currentBeanClass, previousBeanName, i == 0)
-            );
-
-            previousBeanName = currentBeanName;
-        }
-    }
-
-    @SuppressWarnings("deprecation")
-    private AbstractBeanDefinition createBeanDefinition(final Class<?> clazz, final String nextBeanName, final boolean isPrimary) {
-        val beanDefinition = genericBeanDefinition(clazz)
-                .setAutowireMode(AUTOWIRE_AUTODETECT)
-                .getBeanDefinition();
-        beanDefinition.setPrimary(isPrimary);
-        beanDefinition.addQualifier(new AutowireCandidateQualifier(OrderQualifier.class, nextBeanName));
-
-        return beanDefinition;
+    Consumer<OrderConfig<?>> orderConfigProcessor(final BeanDefinitionRegistry beanDefinitionRegistry) {
+        return new OrderConfigProcessor(beanDefinitionRegistry, new BeanDefinitionProvider());
     }
 }
