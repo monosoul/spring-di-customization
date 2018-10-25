20c94e50446b7c01838188a1599f085a7cb0cfce
monosoul <kloz.klaud@gmail.com>
2018-10-26 00:43:20 +0500

    BeanDefinitionProvider: add not null assertion for clazz

--- src/main/java/com/github/monosoul/fortuneteller/spring/order/BeanDefinitionProvider.java
+++ src/main/java/com/github/monosoul/fortuneteller/spring/order/BeanDefinitionProvider.java
@@ -6,12 +6,15 @@ import java.util.function.Function;
 import lombok.val;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.AutowireCandidateQualifier;
+import org.springframework.util.Assert;
 
 final class BeanDefinitionProvider implements Function<BeanDefinitionSpecification, AbstractBeanDefinition> {
 
     @SuppressWarnings("deprecation")
     @Override
     public AbstractBeanDefinition apply(final BeanDefinitionSpecification specification) {
+        Assert.notNull(specification.getClazz(), "Clazz can't be null");
+
         val beanDefinition = genericBeanDefinition(specification.getClazz())
                 .setAutowireMode(AUTOWIRE_AUTODETECT)
                 .getBeanDefinition();
