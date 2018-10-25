e0bfd3537e53687d1436196090fe6098635f6626
monosoul <kloz.klaud@gmail.com>
2018-10-25 17:33:53 +0500

    introduce DecoratorAutowireCandidateResolver using OrderConfig

---
+++ src/main/java/com/github/monosoul/fortuneteller/spring/order/DecoratorAutowireCandidateResolver.java
@@ -0,0 +1,48 @@
+package com.github.monosoul.fortuneteller.spring.order;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.config.DependencyDescriptor;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.AutowireCandidateResolver;
+
+@RequiredArgsConstructor
+public class DecoratorAutowireCandidateResolver implements AutowireCandidateResolver {
+
+    private final AutowireCandidateResolver resolver;
+
+    @Override
+    public boolean isAutowireCandidate(final BeanDefinitionHolder bdHolder, final DependencyDescriptor descriptor) {
+        val dependentType = descriptor.getMember().getDeclaringClass();
+        val dependencyType = descriptor.getDependencyType();
+        val candidateBeanDefinition = (AbstractBeanDefinition) bdHolder.getBeanDefinition();
+
+        if (dependencyType.isAssignableFrom(dependentType) && candidateBeanDefinition.hasBeanClass()
+            && dependencyType.isAssignableFrom(candidateBeanDefinition.getBeanClass())) {
+            val acq = candidateBeanDefinition.getQualifier(OrderQualifier.class.getTypeName());
+            if (acq != null) {
+                if (dependentType.getTypeName().equals(acq.getAttribute("value"))) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        return resolver.isAutowireCandidate(bdHolder, descriptor);
+    }
+
+    @Override
+    public boolean isRequired(final DependencyDescriptor descriptor) {
+        return resolver.isRequired(descriptor);
+    }
+
+    @Override
+    public Object getSuggestedValue(final DependencyDescriptor descriptor) {
+        return resolver.getSuggestedValue(descriptor);
+    }
+
+    @Override
+    public Object getLazyResolutionProxyIfNecessary(final DependencyDescriptor descriptor, final String beanName) {
+        return resolver.getLazyResolutionProxyIfNecessary(descriptor, beanName);
+    }
+}
