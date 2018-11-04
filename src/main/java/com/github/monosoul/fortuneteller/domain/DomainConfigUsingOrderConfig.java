299e075bcb73a049585d1688721fd2fb131326f6
monosoul <kloz.klaud@gmail.com>
2018-11-04 17:07:08 +0500

    update DomainConfigUsingOrderConfig

--- src/main/java/com/github/monosoul/fortuneteller/domain/DomainConfigUsingOrderConfig.java
+++ src/main/java/com/github/monosoul/fortuneteller/domain/DomainConfigUsingOrderConfig.java
@@ -1,11 +1,13 @@
 package com.github.monosoul.fortuneteller.domain;
 
 import static java.util.Arrays.asList;
-import com.github.monosoul.fortuneteller.domain.impl.CachingFortuneTeller;
-import com.github.monosoul.fortuneteller.domain.impl.Globa;
-import com.github.monosoul.fortuneteller.domain.impl.LoggingFortuneTeller;
+import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.CachingFortuneTeller;
+import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.Globa;
+import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.LoggingFortuneTeller;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.CachingHoroscopeTeller;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.Gypsy;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.LoggingHoroscopeTeller;
 import com.github.monosoul.fortuneteller.spring.order.OrderConfig;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +20,15 @@ public class DomainConfigUsingOrderConfig {
                 LoggingFortuneTeller.class,
                 CachingFortuneTeller.class,
                 Globa.class
+        );
+    }
+
+    @Bean
+    public OrderConfig<HoroscopeTeller> horoscopeTellerOrderConfig() {
+        return () -> asList(
+                LoggingHoroscopeTeller.class,
+                CachingHoroscopeTeller.class,
+                Gypsy.class
         );
     }
 }
