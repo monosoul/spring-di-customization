f48a1765fe4072aac95d5becc16a42c22fdaddbf
monosoul <kloz.klaud@gmail.com>
2018-11-04 16:51:39 +0500

    Revert "delete Decorator annotation and it's config"
    This reverts commit 5ed0a92

---
+++ src/main/java/com/github/monosoul/fortuneteller/spring/qualifier/Config.java
@@ -0,0 +1,20 @@
+package com.github.monosoul.fortuneteller.spring.qualifier;
+
+import lombok.val;
+import org.springframework.beans.factory.annotation.CustomAutowireConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static java.util.Set.of;
+
+//@Configuration
+public class Config {
+
+    @Bean
+    public CustomAutowireConfigurer customAutowireConfigurer() {
+        val configurer = new CustomAutowireConfigurer();
+        configurer.setCustomQualifierTypes(of(Decorator.class));
+
+        return configurer;
+    }
+}
