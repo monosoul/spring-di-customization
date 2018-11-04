8297fc7f96a28ba3aabfebc7789c1d7a4168135d
monosoul <kloz.klaud@gmail.com>
2018-11-04 16:20:28 +0500

    update DomainConfig

--- src/main/java/com/github/monosoul/fortuneteller/domain/DomainConfig.java
+++ src/main/java/com/github/monosoul/fortuneteller/domain/DomainConfig.java
@@ -1,17 +1,23 @@
 package com.github.monosoul.fortuneteller.domain;
 
+import com.github.monosoul.fortuneteller.common.ZodiacSign;
 import com.github.monosoul.fortuneteller.da.FortuneResponseRepository;
+import com.github.monosoul.fortuneteller.da.HoroscopeRepository;
 import com.github.monosoul.fortuneteller.da.PersonalDataRepository;
 import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.CachingFortuneTeller;
 import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.Globa;
 import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.LoggingFortuneTeller;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.CachingHoroscopeTeller;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.Gypsy;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.LoggingHoroscopeTeller;
 import com.github.monosoul.fortuneteller.model.FortuneRequest;
 import com.github.monosoul.fortuneteller.model.FortuneResponse;
+import com.github.monosoul.fortuneteller.model.Horoscope;
 import com.github.monosoul.fortuneteller.model.PersonalData;
-import org.springframework.context.annotation.Bean;
-
 import java.util.Map;
 import java.util.function.Function;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 public class DomainConfig {
@@ -26,6 +32,19 @@ public class DomainConfig {
         return new LoggingFortuneTeller(
                 new CachingFortuneTeller(
                         new Globa(fortuneResponseRepository, personalDataExtractor, personalDataRepository),
+                        cache
+                )
+        );
+    }
+
+    @Bean
+    public HoroscopeTeller horoscopeTeller(
+            final Map<ZodiacSign, Horoscope> cache,
+            final HoroscopeRepository horoscopeRepository
+    ) {
+        return new LoggingHoroscopeTeller(
+                new CachingHoroscopeTeller(
+                        new Gypsy(horoscopeRepository),
                         cache
                 )
         );
