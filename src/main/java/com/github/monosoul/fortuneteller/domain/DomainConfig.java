9d3e28f21bfa9d8b51512265cf9d9011a632df7c
monosoul <kloz.klaud@gmail.com>
2018-11-04 17:11:09 +0500

    DomainConfigUsingOrderConfig -> DomainConfig; add test

---
+++ src/main/java/com/github/monosoul/fortuneteller/domain/DomainConfig.java
@@ -0,0 +1,34 @@
+package com.github.monosoul.fortuneteller.domain;
+
+import static java.util.Arrays.asList;
+import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.CachingFortuneTeller;
+import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.Globa;
+import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.LoggingFortuneTeller;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.CachingHoroscopeTeller;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.Gypsy;
+import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.LoggingHoroscopeTeller;
+import com.github.monosoul.fortuneteller.spring.order.OrderConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DomainConfig {
+
+    @Bean
+    public OrderConfig<FortuneTeller> fortuneTellerOrderConfig() {
+        return () -> asList(
+                LoggingFortuneTeller.class,
+                CachingFortuneTeller.class,
+                Globa.class
+        );
+    }
+
+    @Bean
+    public OrderConfig<HoroscopeTeller> horoscopeTellerOrderConfig() {
+        return () -> asList(
+                LoggingHoroscopeTeller.class,
+                CachingHoroscopeTeller.class,
+                Gypsy.class
+        );
+    }
+}
--- src/test/java/com/github/monosoul/fortuneteller/domain/DomainConfigTest.java
+++ src/test/java/com/github/monosoul/fortuneteller/domain/DomainConfigTest.java
@@ -1,31 +1,35 @@
 package com.github.monosoul.fortuneteller.domain;
 
-import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.CachingFortuneTeller;
 import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.Globa;
 import com.github.monosoul.fortuneteller.domain.impl.fortuneteller.LoggingFortuneTeller;
 import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.CachingHoroscopeTeller;
 import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.Gypsy;
 import com.github.monosoul.fortuneteller.domain.impl.horoscopeteller.LoggingHoroscopeTeller;
-import com.github.monosoul.fortuneteller.spring.order.OrderConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import lombok.val;
+import org.junit.jupiter.api.Test;
 
-@Configuration
-public class DomainConfigUsingOrderConfig {
+class DomainConfigTest {
 
-    @Bean
-    public OrderConfig<FortuneTeller> fortuneTellerOrderConfig() {
-        return () -> asList(
+    @Test
+    void fortuneTellerOrderConfig() {
+        val actual = new DomainConfig().fortuneTellerOrderConfig();
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.getClasses()).containsExactly(
                 LoggingFortuneTeller.class,
                 CachingFortuneTeller.class,
                 Globa.class
         );
     }
 
-    @Bean
-    public OrderConfig<HoroscopeTeller> horoscopeTellerOrderConfig() {
-        return () -> asList(
+    @Test
+    void horoscopeTellerOrderConfig() {
+        val actual = new DomainConfig().horoscopeTellerOrderConfig();
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.getClasses()).containsExactly(
                 LoggingHoroscopeTeller.class,
                 CachingHoroscopeTeller.class,
                 Gypsy.class
