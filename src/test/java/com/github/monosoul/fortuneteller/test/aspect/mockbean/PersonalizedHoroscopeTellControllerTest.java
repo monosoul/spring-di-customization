ae829a484d9227ab64e7dc058553eefac51f12f2
monosoul <kloz.klaud@gmail.com>
2018-11-04 23:02:39 +0500

    update tests to reflect changes to the aspect

--- src/test/java/com/github/monosoul/fortuneteller/test/aspect/mockbean/PersonalizedHoroscopeTellControllerTest.java
+++ src/test/java/com/github/monosoul/fortuneteller/test/aspect/mockbean/PersonalizedHoroscopeTellControllerTest.java
@@ -1,14 +1,17 @@
 package com.github.monosoul.fortuneteller.test.aspect.mockbean;
 
-import static com.github.monosoul.fortuneteller.aspect.TellTheTruthAspect.THE_TRUTH;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
-import static org.assertj.core.api.Assertions.assertThat;
-import com.github.monosoul.fortuneteller.aspect.TellTheTruthAspect;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import com.github.monosoul.fortuneteller.aspect.AccessDeniedException;
+import com.github.monosoul.fortuneteller.aspect.RestrictionAspect;
 import com.github.monosoul.fortuneteller.common.ZodiacSign;
 import com.github.monosoul.fortuneteller.domain.HoroscopeTeller;
+import com.github.monosoul.fortuneteller.test.aspect.RequestContextHolderConfigurer;
 import com.github.monosoul.fortuneteller.web.PersonalizedHoroscopeTellController;
 import java.util.function.Function;
-import lombok.val;
+import java.util.function.Predicate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,7 +23,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig
-@ActiveProfiles({"unit-mockbean", "tellTheTruth"})
+@ActiveProfiles({"unit", "mockbean", "restrictAccess"})
 public class PersonalizedHoroscopeTellControllerTest {
 
     private static final int LIMIT = 10;
@@ -31,21 +34,29 @@ public class PersonalizedHoroscopeTellControllerTest {
     private Function<String, ZodiacSign> zodiacSignConverter;
     @MockBean
     private Function<String, String> nameNormalizer;
+    @MockBean
+    private Predicate<String> ipIsAllowed;
     @Autowired
     private PersonalizedHoroscopeTellController controller;
 
     @Test
-    void tellTheTruth() {
-        val actual = controller.tell(randomAlphabetic(LIMIT), randomAlphabetic(LIMIT));
+    void doNothingWhenAllowed() {
+        when(ipIsAllowed.test(anyString())).thenReturn(true);
 
-        assertThat(actual).isNotNull();
-        assertThat(actual.getName()).isEqualTo("Anonymous");
-        assertThat(actual.getHoroscope().getMessage()).isEqualTo(THE_TRUTH);
+        controller.tell(randomAlphabetic(LIMIT), randomAlphabetic(LIMIT));
+    }
+
+    @Test
+    void throwExceptionWhenNotAllowed() {
+        when(ipIsAllowed.test(anyString())).thenReturn(false);
+
+        assertThatThrownBy(() -> controller.tell(randomAlphabetic(LIMIT), randomAlphabetic(LIMIT)))
+                .isInstanceOf(AccessDeniedException.class);
     }
 
     @Configuration
-    @Profile("unit-mockbean")
-    @Import({PersonalizedHoroscopeTellController.class, TellTheTruthAspect.class})
+    @Profile({"unit", "mockbean"})
+    @Import({PersonalizedHoroscopeTellController.class, RestrictionAspect.class, RequestContextHolderConfigurer.class})
     @EnableAspectJAutoProxy
     public static class Config {
 
