c58d08cff4331cef08de7ede825cfc43a889fff5
monosoul <kloz.klaud@gmail.com>
2018-11-04 20:22:59 +0500

    simplify DecoratorType

--- src/main/java/com/github/monosoul/fortuneteller/spring/DecoratorType.java
+++ src/main/java/com/github/monosoul/fortuneteller/spring/DecoratorType.java
@@ -1,19 +1,7 @@
 package com.github.monosoul.fortuneteller.spring;
 
-import lombok.NonNull;
-
 public enum DecoratorType {
-    LOGGING("Logging"),
-    CACHING("Caching"),
-    NOT_DECORATOR("");
-
-    private final String prefix;
-
-    DecoratorType(@NonNull final String prefix) {
-        this.prefix = prefix;
-    }
-
-    public String getPrefix() {
-        return prefix;
-    }
+    LOGGING,
+    CACHING,
+    NOT_DECORATOR
 }
