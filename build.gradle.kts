8d3f781eaaba14056157e4672d9b022f8ae432af
monosoul <kloz.klaud@gmail.com>
2018-11-08 23:41:31 +0500

    increase libs versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val byteBuddyVersion by extra { "1.9.2" }
 val junitVersion by extra { "5.3.1" }
 
 buildscript {
-    val springBootVersion by extra { "2.0.5.RELEASE" }
+    val springBootVersion by extra { "2.1.0.RELEASE" }
 
     repositories {
         mavenCentral()
@@ -41,12 +41,12 @@ dependencies {
     compileOnly(lombokDependency)
 
     testCompileOnly(lombokDependency)
-    testCompile("org.apache.commons:commons-lang3:3.8")
+    testCompile("org.apache.commons:commons-lang3:3.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testRuntime("org.junit.vintage:junit-vintage-engine:$junitVersion")
-    testCompile("org.assertj:assertj-core:3.9.1")
+    testCompile("org.assertj:assertj-core:3.11.1")
     testCompile("org.mockito:mockito-core:2.23.0") {
         exclude("net.bytebuddy")
     }
